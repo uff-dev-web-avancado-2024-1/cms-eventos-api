630dfbb1692a1fca71a67ebeaa98634ffba3d941
Igor Deo Alves <igordeo@id.uff.br>
2024-05-19 14:19:40 -0300

    chore: setup inicial com swagger e banco

---
+++ src/main/java/com/example/cmseventosapi/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.example.cmseventosapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}

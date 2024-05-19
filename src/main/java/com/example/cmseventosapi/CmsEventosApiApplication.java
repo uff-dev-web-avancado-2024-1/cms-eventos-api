29222cfe376c63f2ed1223d6096fac58812626aa
Igor Deo Alves <igordeo@id.uff.br>
2024-05-19 12:23:07 -0300

    chore: intelliJ springboot project setup

---
+++ src/main/java/com/example/cmseventosapi/CmsEventosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmseventosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsEventosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsEventosApiApplication.class, args);
+    }
+
+}

4de6d20b2b1612ca7f94cfaa34e4c743b7750555
BrunoMontate <bruno_montate@id.uff.br>
2024-05-30 14:11:46 -0300

    Criando enpoints de cadastro de espaco e mensagem no user

--- src/main/java/com/example/cmseventosapi/Controllers/SpaceController.java
+++ src/main/java/com/example/cmseventosapi/Controllers/SpaceController.java
@@ -1,8 +1,30 @@
 package com.example.cmseventosapi.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cmseventosapi.Model.Space;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@RequestMapping("/api/espaco")
+@Tag(name = "cms-eventos-api")
 public class SpaceController {
-    
+
+    @Operation(summary = "Cadastrar espaço disponivel", method = "POST")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Espaço cadastrado com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Parâmetros inválidos para o cadastro do espaço"),
+        @ApiResponse(responseCode = "500", description = "Erro ao cadastrar espaço")
+    })
+    @PostMapping(consumes = org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> cadastrarEspacoDisponivel(@RequestPart MultipartFile file) {
+        //TODO: process POST request     
+        return new ResponseEntity<>(null);
+    }
+
 }
--- src/main/java/com/example/cmseventosapi/Controllers/UserController.java
+++ src/main/java/com/example/cmseventosapi/Controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.example.cmseventosapi.Controllers;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 
 @RestController
@@ -27,6 +31,16 @@ public class UserController {
         return "Usuário cadastrado com Sucesso";
     }
 
+    @Operation(summary = "Enviar mensagem com atividades favoritas", method = "POST")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Mensagem enviada com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro ao enviar mensagem")
+    })
+    @PostMapping(consumes = org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> enviarMesagemFavoritas(@RequestPart MultipartFile file) {
+        //TODO: process POST request     
+        return new ResponseEntity<>(null);
+    }
 
 
 }

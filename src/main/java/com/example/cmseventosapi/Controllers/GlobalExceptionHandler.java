8a20131e9e435640d46350992531294de35c883a
Igor Deo Alves <igordeo@id.uff.br>
2024-07-02 19:50:43 -0300

    feat: handle not found globally

--- src/main/java/com/example/cmseventosapi/Controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/cmseventosapi/Controllers/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.webjars.NotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +19,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return new ResponseEntity<>("Not Found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
